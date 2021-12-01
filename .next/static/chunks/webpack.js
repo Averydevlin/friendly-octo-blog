4f98f09d70a93b53a5a994109748db924ef17322
Averydevlin <56742149+Averydevlin@users.noreply.github.com>
2021-11-30 20:39:15 -0800

    navbar

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "c62a4262bc226563"; }
+/******/ 		__webpack_require__.h = function() { return "e478deed9122c034"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,18 +1,21 @@
 import Link from 'next/link'
 import Head from 'next/head'
+import Layout from '../../components/layout'
+import Navbar from '../../components/navbar'
 
 export default function FirstPost() {
   return (
-    <>
-    <Head>
-      <title> First Post</title>
-    </Head>
+    <Layout>
+      <Navbar></Navbar>
+      <Head>
+        <title>First Post</title>
+      </Head>
       <h1>First Post</h1>
       <h2>
         <Link href="/">
           <a>Back to home</a>
         </Link>
       </h2>
-    </>
+    </Layout>
   )
 }
